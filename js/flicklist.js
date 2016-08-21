7cf3aba8c1c4b99eeb1c4b85843b63e001353a29
Steve Barlow <stevedbarlow@me.com>
2016-08-21 14:25:48 +0000

    finish FlickList 5 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -2,16 +2,17 @@
 
 var model = {
   watchlistItems: [],
-  browseItems: []
+  browseItems: [],
 
   // TODO 
   // add a property for the current active movie index
+  activeMovieIndex: 0
 }
 
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
+  token: "e676a379af774c11c308c852c89464d8", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
@@ -83,7 +84,7 @@ function render() {
 
   // clear everything
   $("#section-watchlist ul").empty();
-  $("#section-browse ul").empty();
+  $("#section-browse .carousel-inner").empty();
 
   // render watchlist items
   model.watchlistItems.forEach(function(movie) {
@@ -122,6 +123,40 @@ function render() {
     $("#section-watchlist ul").append(itemView);
   });
 
+  var activeMovie = model.browseItems[model.activeMovieIndex];
+  
+  $("#browse-info h4").text(activeMovie.original_title);
+  $("#browse-info p").text(activeMovie.overview);  
+  $("#add-to-watchlist")
+    .attr("class", "btn btn-primary")
+    .unbind("click")
+    .click(function() {
+      model.watchlistItems.push(activeMovie);
+      render();
+    })
+    .prop("disabled", model.watchlistItems.indexOf(activeMovie) !== -1);
+
+  var posters = model.browseItems.map(function(movie) {
+    var poster = $("<img></img>")
+      .attr("src", api.posterUrl(movie))
+      .attr("class", "img-responsive");
+
+    return $("<li></li>")
+      .attr("class", "item")
+      .append(poster);
+  });
+  $("#section-browse .carousel-inner").append(posters);
+  posters[model.activeMovieIndex].addClass("active");
+}
+
+
+// When the HTML document is ready, we call the discoverMovies function,
+// and pass the render function as its callback
+$(document).ready(function() {
+  discoverMovies(render);
+});
+
+/*
   // render browse items
   model.browseItems.forEach(function(movie) {
     var title = $("<h4></h4>").text(movie.original_title);
@@ -144,11 +179,4 @@ function render() {
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
-}
-
-
-// When the HTML document is ready, we call the discoverMovies function,
-// and pass the render function as its callback
-$(document).ready(function() {
-  discoverMovies(render);
-});
+*/
