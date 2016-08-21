c974a229679c7f74c047e4b11ddf6e76abab1ca7
Steve Barlow <stevedbarlow@me.com>
2016-08-21 10:13:03 +0000

    finish FlickList 1 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "e676a379af774c11c308c852c89464d8" // TODO 0 put your api key here
 }
 
 
@@ -19,6 +19,7 @@ var api = {
  */
 function discoverMovies(callback) {
 	$.ajax({
+		
 		url: api.root + "/discover/movie",
 		data: {
 			api_key: api.token,
@@ -29,7 +30,8 @@ function discoverMovies(callback) {
 			
 			// TODO 2
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
+			model.browseItems = response.results;
+		
 			// invoke the callback function that was passed in. 
 			callback();
 		}
@@ -44,20 +46,41 @@ function discoverMovies(callback) {
 function render() {
   // TODO 7
   // clear everything from both lists
+  $("#section-watchlist ul").empty();
+  $("#section-browse ul").empty();  
   
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
+  model.watchlistItems.forEach(function(movie) {
+  	var listItem = $("<li></li>").text(movie.original_title);
+  	
+  	$("#section-watchlist ul").append(listItem);
+  	
+  })
   
   // for each movie on the current browse list, 
   model.browseItems.forEach(function(movie) {
 		// TODO 3
 		// insert a list item into the <ul> in the browse section
+		var title = $("<p></p>").text(movie.original_title);
+		var itemView = $("<li></li>").append(title);
+
+		$("#section-browse ul").append(itemView);
 		
 		// TODO 4
 		// the list item should include a button that says "Add to Watchlist"
 		
+		var button = $("<button></button>").text("Add to Watchlist").click(function() {
+			
 		// TODO 5
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+		
+			model.watchlistItems.push(movie);
+			render();
+		
+		});
+		itemView.append(button);
+		
   });
   
 }
